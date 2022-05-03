e307ae88d7b1d98e18096c49e8cebd0ac8532742
Omri <67146455+omriberchman@users.noreply.github.com>
2022-05-03 18:31:37 +0300

    Add files via upload

---
+++ parentsim.py
@@ -0,0 +1,37 @@
+import os
+
+print("00000000000000000000000000000000000000000000000")
+print("000000000000000 Parent Simulator 00000000000000")
+print("00000000000000000000000000000000000000000000000")
+
+print('')
+print("Good'ay son!")
+print("")
+print('How was school today?')
+print('1. Excellent, had a very informative day!')
+print('2. It was ok, not something special.')
+print('3. meh.. it was pretty boring...')
+answer1 = input("Insert your answer here [IN NUMBERS!] : ")
+
+if answer1 == '1':
+
+    print('')
+    print('')
+    print("Why so?")
+    print("1. I got A+ in test")
+    print("2. We had an interesting lecture.")
+    print("3. I got a kiss!")
+    if1 = input("Insert your answer [IN NUMBERS!] ? ")
+    if if1 == "1":
+        print("Very good son.")
+elif if1 == "2":
+    print('')
+    if11 = input("What was it about?")
+elif if1 == "3":
+    print("~~YOU DIED~~")
+    os.system('pause')
+
+elif answer1 == '2':
+    if2
+
+os.system('pause')
